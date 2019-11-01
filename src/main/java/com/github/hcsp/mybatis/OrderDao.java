d5d65326bb42aa81d737fa9cc3ddf2d235a7c4fb
huangjiaxi <huangjiaxi@oak-inc.com>
2019-11-01 11:43:02 +0800

    myBatis

--- src/main/java/com/github/hcsp/mybatis/OrderDao.java
+++ src/main/java/com/github/hcsp/mybatis/OrderDao.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.mybatis;
 
 import com.github.hcsp.mybatis.entity.Order;
+
 import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 public class OrderDao {
@@ -18,6 +21,9 @@ public class OrderDao {
      * @return 所有的订单信息
      */
     public List<Order> getInnerJoinOrders() {
-        return null;
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            List<Order> res = session.selectList("MyMapper.getInnerJoinOrders");
+            return res;
+        }
     }
 }
