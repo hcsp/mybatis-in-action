e3ba9b8eba852de45e84f4e0a61b07cb2d3c0549
lolo <53633837+sun1341283@users.noreply.github.com>
2020-03-31 18:14:19 +0800

    完成mybatis进行crud (#46)
    完成mybatis进行crud (#46)

---
+++ src/main/java/com/github/hcsp/mybatis/mapper/OrderMapper.java
@@ -0,0 +1,9 @@
+package com.github.hcsp.mybatis.mapper;
+
+import com.github.hcsp.mybatis.entity.Order;
+
+import java.util.List;
+
+public interface OrderMapper {
+    List<Order> selectOrders();
+}
