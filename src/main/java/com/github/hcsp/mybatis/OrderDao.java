f402997c36ff5be84254d7105d53de74f38d41ce
leo2c <z429882801@qq.com>
2020-05-07 22:53:31 +0800

    Mybatis练习

--- src/main/java/com/github/hcsp/mybatis/OrderDao.java
+++ src/main/java/com/github/hcsp/mybatis/OrderDao.java
@@ -1,11 +1,15 @@
 package com.github.hcsp.mybatis;
 
 import com.github.hcsp.mybatis.entity.Order;
+
 import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 public class OrderDao {
     private final SqlSessionFactory sqlSessionFactory;
+    public static final String NAME_SPACE = "com.hcsp.OrderMapper";
 
     public OrderDao(SqlSessionFactory sqlSessionFactory) {
         this.sqlSessionFactory = sqlSessionFactory;
@@ -18,6 +22,8 @@ public class OrderDao {
      * @return 所有的订单信息
      */
     public List<Order> getInnerJoinOrders() {
-        return null;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return sqlSession.selectList(NAME_SPACE + ".getInnerJoinOrders");
+        }
     }
 }
