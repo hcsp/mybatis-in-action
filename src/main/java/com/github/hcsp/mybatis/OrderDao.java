c2505efdb6879d0d83092acd59656c91bee6911e
Sunpeng526 <215860865@qq.com>
2020-06-24 02:02:22 -0700

    mybatis learn 1

--- src/main/java/com/github/hcsp/mybatis/OrderDao.java
+++ src/main/java/com/github/hcsp/mybatis/OrderDao.java
@@ -2,6 +2,8 @@ package com.github.hcsp.mybatis;
 
 import com.github.hcsp.mybatis.entity.Order;
 import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 public class OrderDao {
@@ -18,6 +20,10 @@ public class OrderDao {
      * @return 所有的订单信息
      */
     public List<Order> getInnerJoinOrders() {
-        return null;
+        List<Order> result = null;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            result = session.selectList("com.github.hcsp.mybatis.getInnerJoinOrders");
+        }
+        return result;
     }
 }
