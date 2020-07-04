af778a8882da65465c078adce7a5b3db6b60872d
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-07-05 00:03:28 +0800

    完成mybatis实战 (#66)
    完成mybatis实战 (#66)

--- src/main/java/com/github/hcsp/mybatis/UserDao.java
+++ src/main/java/com/github/hcsp/mybatis/UserDao.java
@@ -2,10 +2,20 @@ package com.github.hcsp.mybatis;
 
 import com.github.hcsp.mybatis.entity.Pagination;
 import com.github.hcsp.mybatis.entity.User;
-import java.util.List;
-import org.apache.ibatis.session.SqlSessionFactory;
 
-/** 与用户有关的增删改查操作 */
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * 与用户有关的增删改查操作
+ */
 public class UserDao {
     private final SqlSessionFactory sqlSessionFactory;
 
@@ -18,11 +28,20 @@ public class UserDao {
      *
      * @param username 传入的用户名
      * @param pageSize 分页搜索，每页显示的条数
-     * @param pageNum 分页的页码，从1开始
+     * @param pageNum  分页的页码，从1开始
      * @return 查找结果，若username为null，则返回所有用户的列表
      */
     public Pagination<User> getUserByPage(String username, int pageSize, int pageNum) {
-        return null;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("username", username);
+            map.put("offset", (pageNum - 1) * pageSize);
+            map.put("limit", pageSize);
+            List<User> users = session.selectList("myMapper.getUser", map);
+            int count = session.selectOne("myMapper.countUser", username);
+            int totalPage = count % pageSize == 0 ? count / pageSize : count / pageSize + 1;
+            return Pagination.pageOf(users, pageSize, pageNum, totalPage);
+        }
     }
 
     /**
@@ -30,21 +49,35 @@ public class UserDao {
      *
      * @param users 待插入的用户列表
      */
-    public void batchInsertUsers(List<User> users) {}
+    public void batchInsertUsers(List<User> users) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("users", users);
+            session.insert("myMapper.batchInsertUsers", map);
+        }
+    }
 
     /**
      * 根据用户的ID更新一个用户的数据，更新传入的user中所有不为null的字段。
      *
      * @param user 要修改的用户信息，其id必须不为null
      */
-    public void updateUser(User user) {}
+    public void updateUser(User user) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.update("myMapper.updateUser", user);
+        }
+    }
 
     /**
      * 删除一个用户。
      *
      * @param id 待删除的用户ID
      */
-    public void deleteUserById(Integer id) {}
+    public void deleteUserById(Integer id) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.delete("myMapper.deleteUserById", id);
+        }
+    }
 
     /**
      * 根据ID获取一个用户，如果该用户不存在，返回null
@@ -53,6 +86,31 @@ public class UserDao {
      * @return 对应的用户
      */
     public User selectUserById(Integer id) {
-        return null;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            return session.selectOne("myMapper.selectUserById", id);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String resource = "db/mybatis/config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory build = new SqlSessionFactoryBuilder().build(inputStream);
+        UserDao userDao = new UserDao(build);
+
+//        System.out.println(userDao.getUserByPage("zhangsan", 1, 10));
+
+//        User user2 = new User(122, "1cvcv", "lisi1111j", "xxxxxx");
+//        User user3 = new User(132, "1dfdfdgfdfdf", "lisi11111", "xxxxxx");
+//
+//        List<User> users = new ArrayList<>();
+//        users.add(user1);
+//        users.add(user2);
+//        users.add(user3);
+//
+//        userDao.batchInsertUsers(users);
+
+//        User user1 = new User(1, "12341ererererererer", null, null);
+//        userDao.updateUser(user1);
+
     }
 }
