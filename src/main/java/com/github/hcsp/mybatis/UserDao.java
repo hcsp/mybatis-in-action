5794a5940bf42402f35c624270d60328b2e6e4ee
McDul <907420318@qq.com>
2020-08-02 15:40:25 +0800

    熟悉JDBC、ORM以及MyBatis的各种概念 (#70)
    熟悉JDBC、ORM以及MyBatis的各种概念 (#70)

--- src/main/java/com/github/hcsp/mybatis/UserDao.java
+++ src/main/java/com/github/hcsp/mybatis/UserDao.java
@@ -2,10 +2,17 @@ package com.github.hcsp.mybatis;
 
 import com.github.hcsp.mybatis.entity.Pagination;
 import com.github.hcsp.mybatis.entity.User;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-/** 与用户有关的增删改查操作 */
+/**
+ * 与用户有关的增删改查操作
+ */
 public class UserDao {
     private final SqlSessionFactory sqlSessionFactory;
 
@@ -18,11 +25,22 @@ public class UserDao {
      *
      * @param username 传入的用户名
      * @param pageSize 分页搜索，每页显示的条数
-     * @param pageNum 分页的页码，从1开始
+     * @param pageNum  分页的页码，从1开始
      * @return 查找结果，若username为null，则返回所有用户的列表
      */
     public Pagination<User> getUserByPage(String username, int pageSize, int pageNum) {
-        return null;
+
+
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("username", username);
+            map.put("offset", (pageNum - 1) * pageSize);
+            map.put("limit", pageSize);
+            List<User> users = session.selectList("UserMapper.getUserByPage", map);
+            int count = session.selectOne("UserMapper.countUser", username);
+            int totalPage = count % pageSize == 0 ? count / pageSize : count / pageSize + 1;
+            return Pagination.pageOf(users, pageSize, pageNum, totalPage);
+        }
     }
 
     /**
@@ -30,21 +48,35 @@ public class UserDao {
      *
      * @param users 待插入的用户列表
      */
-    public void batchInsertUsers(List<User> users) {}
+    public void batchInsertUsers(List<User> users) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            Map<String, Object> param = new HashMap<>();
+            param.put("users", users);
+            sqlSession.insert("UserMapper.batchInsertUsers", param);
+        }
+    }
 
     /**
      * 根据用户的ID更新一个用户的数据，更新传入的user中所有不为null的字段。
      *
      * @param user 要修改的用户信息，其id必须不为null
      */
-    public void updateUser(User user) {}
+    public void updateUser(User user) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            sqlSession.update("UserMapper.updateUser", user);
+        }
+    }
 
     /**
      * 删除一个用户。
      *
      * @param id 待删除的用户ID
      */
-    public void deleteUserById(Integer id) {}
+    public void deleteUserById(Integer id) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            sqlSession.delete("UserMapper.deleteUserById", id);
+        }
+    }
 
     /**
      * 根据ID获取一个用户，如果该用户不存在，返回null
@@ -53,6 +85,8 @@ public class UserDao {
      * @return 对应的用户
      */
     public User selectUserById(Integer id) {
-        return null;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            return sqlSession.selectOne("UserMapper.selectUserById", id);
+        }
     }
 }
