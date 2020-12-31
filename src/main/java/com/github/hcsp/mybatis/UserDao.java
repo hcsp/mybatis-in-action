3b340344db2c0861722b02237f8005e8d3b46ccd
iRuby <1479867360@qq.com>
2020-12-31 21:38:49 +0800

    我就不该去格式化....

--- src/main/java/com/github/hcsp/mybatis/UserDao.java
+++ src/main/java/com/github/hcsp/mybatis/UserDao.java
@@ -2,10 +2,17 @@ package com.github.hcsp.mybatis;
 
 import com.github.hcsp.mybatis.entity.Pagination;
 import com.github.hcsp.mybatis.entity.User;
-import java.util.List;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-/** 与用户有关的增删改查操作 */
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 与用户有关的增删改查操作
+ */
 public class UserDao {
     private final SqlSessionFactory sqlSessionFactory;
 
@@ -18,11 +25,18 @@ public class UserDao {
      *
      * @param username 传入的用户名
      * @param pageSize 分页搜索，每页显示的条数
-     * @param pageNum 分页的页码，从1开始
+     * @param pageNum  分页的页码，从1开始
      * @return 查找结果，若username为null，则返回所有用户的列表
      */
     public Pagination<User> getUserByPage(String username, int pageSize, int pageNum) {
-        return null;
+        SqlSession session = sqlSessionFactory.openSession(true);
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", username);
+        map.put("pageSize", pageSize);
+        map.put("offset", (pageNum - 1) * pageSize);
+        int totalPage = session.selectOne("com.github.hcsp.mybatis.UserDao.getTotalPage", map);
+        List<User> list = session.selectList("com.github.hcsp.mybatis.UserDao.getUserList", map);
+        return Pagination.pageOf(list, pageSize, pageNum, totalPage);
     }
 
     /**
@@ -30,21 +44,35 @@ public class UserDao {
      *
      * @param users 待插入的用户列表
      */
-    public void batchInsertUsers(List<User> users) {}
+    public void batchInsertUsers(List<User> users) {
+        SqlSession session = sqlSessionFactory.openSession(ExecutorType.BATCH, false);
+        for (User user : users) {
+            session.insert("com.github.hcsp.mybatis.UserDao.batchInsertUsers", user);
+        }
+        session.commit();
+    }
 
     /**
      * 根据用户的ID更新一个用户的数据，更新传入的user中所有不为null的字段。
      *
      * @param user 要修改的用户信息，其id必须不为null
      */
-    public void updateUser(User user) {}
+    public void updateUser(User user) {
+        if (user.getId() != null) {
+            SqlSession session = sqlSessionFactory.openSession(true);
+            System.out.println(session.update("com.github.hcsp.mybatis.UserDao.updateUser", user));
+        }
+    }
 
     /**
      * 删除一个用户。
      *
      * @param id 待删除的用户ID
      */
-    public void deleteUserById(Integer id) {}
+    public void deleteUserById(Integer id) {
+        SqlSession session = sqlSessionFactory.openSession(true);
+        session.delete("com.github.hcsp.mybatis.UserDao.deleteUserById", id);
+    }
 
     /**
      * 根据ID获取一个用户，如果该用户不存在，返回null
@@ -53,6 +81,7 @@ public class UserDao {
      * @return 对应的用户
      */
     public User selectUserById(Integer id) {
-        return null;
+        SqlSession session = sqlSessionFactory.openSession(true);
+        return session.selectOne("com.github.hcsp.mybatis.UserDao.selectUserById", id);
     }
 }
